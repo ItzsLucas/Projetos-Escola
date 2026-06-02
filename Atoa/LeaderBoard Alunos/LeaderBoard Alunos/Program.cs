5e20671b217771942cc3cae274588a201f394d55
Lucas Rocha <2010lucasrocha@gmail.com>
2026-06-02 20:17:02 +0100

    Adiciona projeto inicial LeaderBoard Alunos
    - Adicionado arquivo de solução (`LeaderBoard Alunos.slnx`).
    - Criado arquivo de configuração (`App.config`) para runtime .NET 4.7.2.
    - Adicionado arquivo de projeto (`LeaderBoard Alunos.csproj`) com:
      - Configurações do MSBuild para Debug e Release.
      - Referências a bibliotecas padrão do .NET.
      - Inclusão de arquivos principais no projeto.
    - Implementado sistema de gerenciamento de alunos em `Program.cs`:
      - Adicionar e remover alunos.
      - Validação de entradas e menu interativo.
    - Adicionado `AssemblyInfo.cs` com metadados do assembly.

---
+++ Atoa/LeaderBoard Alunos/LeaderBoard Alunos/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBoard_Alunos
+{
+    internal class Program
+    {
+        static public void Digite()
+        {
+            Console.WriteLine("Digite slgo para avançar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+            List<float> notas = new List<float>();
+            List<string> nomes = new List<string>();
+            int Pescolha;
+            string nome, NomeRemover;
+            float nota;
+            do
+            {
+
+                Console.WriteLine("======= Gestor da Turma =======");
+                Console.Write("\nDigite\n [1] Para adicionar aluno\n [2] Para remover aluno\n [3] Para pesquisar aluno\n [4] Para listar alunos\n [5] Para procurar aluno\n [0] Para sair\nOpção: ");
+                do
+                {
+                    while (!int.TryParse(Console.ReadLine(), out Pescolha))
+                    {
+                        Console.WriteLine("Digite apenas numeros inteiros");
+                    }
+                    if (Pescolha < 0 || Pescolha > 5)
+                    {
+                        Console.WriteLine("Digite apenas numeros entre [0-5]");
+                    }
+                    Console.Write("\nOpção: ");
+                } while (Pescolha < 0 || Pescolha > 5);
+                Console.Clear();
+                switch(Pescolha)
+                {
+                    case 1:
+                        Console.WriteLine("======= Adicionar Aluno =======");
+                        Console.Write("Digite o nome do aluno: ");
+                        nome = Console.ReadLine();
+                        Console.Write("Digite a nota do aluno: ");
+
+                        do
+                        {
+                            while (!float.TryParse(Console.ReadLine(), out nota))
+                            {
+                                Console.WriteLine("Digite apenas numeros");
+                            }
+                            if (nota > 20 || nota < 0)
+                            {
+                                Console.WriteLine("Digite apenas numeros entre [0-20]");
+                            }
+                            Console.Write("\nOpção: ");
+
+                        } while (nota > 20 || nota < 0);
+
+                        Console.WriteLine("======= Adicionar Aluno =======");
+                        nomes.Add(nome);
+                        notas.Add(nota);
+                        Console.WriteLine($"O aluno {nome} foi adicionado com a nota {nota}");
+                        Digite();
+                        break;
+
+                    case 2:
+                        if (nomes.Count < 1)
+                        {
+                            Console.WriteLine("É necessário ter alunos registrados par usar esta funçao");
+                        }
+                        else
+                        {
+                            int contador = 0, contador2 = 0;
+                            Console.WriteLine("======= Remover Aluno =======");
+                            Console.Write("\nDigite o nome do aluno que deseja remover: ");
+                            NomeRemover = Console.ReadLine();
+                            foreach(string item in nomes)
+                            {
+                                if(NomeRemover == item)
+                                {
+                                    nomes.Remove(NomeRemover);
+                                }
+                                else
+                                {
+                                    contador++;
+                                }
+                            }
+                            foreach(float item in notas)
+                            {
+                                if (contador == contador2)
+                                {
+                                    notas.Remove(item);
+                                }
+
+                                else
+                                {
+                                    contador2++;
+                                }
+                            }
+                        }
+                        Digite();
+
+                        break;
+                }
+            } while (Pescolha != 0);
+        }
+    }
+}
---
+++ Atoa/LeaderBoard Alunos/LeaderBoard Alunos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("LeaderBoard Alunos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeaderBoard Alunos")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("645640c4-bdca-415c-b9a4-1299c4556508")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
